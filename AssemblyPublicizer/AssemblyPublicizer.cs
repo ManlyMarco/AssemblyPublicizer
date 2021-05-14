6cc4b23c14b4630cf749f67998e1be7df4e07f24
ManlyMarco <dummyaccx@gmail.com>
2021-05-14 18:21:27 +0200

    Allow passing directories

--- AssemblyPublicizer/AssemblyPublicizer.cs
+++ AssemblyPublicizer/AssemblyPublicizer.cs
@@ -56,103 +56,17 @@ namespace CabbageCrow.AssemblyPublicizer
 
 			foreach (string input in args)
 			{
-				var assName = Path.GetFileNameWithoutExtension(input);
-				var outputName = assName + suffix + Path.GetExtension(input);
-				Console.WriteLine($"Info: Processing Assembly {assName}...");
-
-				AssemblyDefinition assembly = null;
-
-				if (!File.Exists(input))
+				if (Directory.Exists(input))
 				{
-					Console.WriteLine();
-					Console.WriteLine("ERROR! File doesn't exist or you don't have sufficient permissions.");
-					Exit(30);
-				}
-
-				try
-				{
-					assembly = AssemblyDefinition.ReadAssembly((string)input);
-				}
-				catch (Exception)
-				{
-					Console.WriteLine();
-					Console.WriteLine("ERROR! Cannot read the assembly. Please check your permissions.");
-					Exit(40);
-				}
-
-
-				var allTypes = GetAllTypes(assembly.MainModule);
-				var allMethods = allTypes.SelectMany(t => t.Methods);
-
-				var allFields = FilterBackingEventFields(allTypes);
-
-				int count;
-				string reportString = "Changed {0} {1} to public.";
-
-				#region Make everything public
-
-				count = 0;
-				foreach (var type in allTypes)
-				{
-					if (!type?.IsPublic ?? false && !type.IsNestedPublic)
+					foreach (var file in Directory.GetFiles(input, "*.dll", SearchOption.AllDirectories))
 					{
-						count++;
-						if (type.IsNested)
-							type.IsNestedPublic = true;
-						else
-							type.IsPublic = true;
+						ProcessAssembly(file, suffix, outputPath);
 					}
 				}
-
-				Console.WriteLine(reportString, count, "types");
-
-				count = 0;
-				foreach (var method in allMethods)
+				else
 				{
-					if (!method?.IsPublic ?? false)
-					{
-						count++;
-						method.IsPublic = true;
-					}
+					ProcessAssembly(input, suffix, outputPath);
 				}
-				Console.WriteLine(reportString, count, "methods (including getters and setters)");
-
-				count = 0;
-				foreach (var field in allFields)
-				{
-					if (!field?.IsPublic ?? false)
-					{
-						count++;
-						field.IsPublic = true;
-					}
-				}
-				Console.WriteLine(reportString, count, "fields");
-
-				#endregion
-
-
-				Console.WriteLine();
-
-				Console.Write("Saving a copy of the modified assembly ...");
-
-				var outputFile = Path.Combine(outputPath, outputName);
-
-				try
-				{
-					if (outputPath != "" && !Directory.Exists(outputPath))
-						Directory.CreateDirectory(outputPath);
-					assembly.Write(outputFile);
-				}
-				catch (Exception)
-				{
-					Console.WriteLine();
-					Console.WriteLine("ERROR! Cannot create/overwrite the new assembly. ");
-					Console.WriteLine("Please check the path and its permissions " +
-						"and in case of overwriting an existing file ensure that it isn't currently used.");
-					Exit(50);
-				}
-				Console.WriteLine(" OK");
-				Console.WriteLine();
 			}
 
 			Console.WriteLine("Completed.");
@@ -163,6 +77,110 @@ namespace CabbageCrow.AssemblyPublicizer
 			Console.WriteLine("private members except for types.");
 
 			Exit(0);
+		}
+
+		private static void ProcessAssembly(string input, string suffix, string outputPath)
+		{
+			var assName = Path.GetFileNameWithoutExtension(input);
+			var outputName = assName + suffix + Path.GetExtension(input);
+			Console.WriteLine($"Info: Processing Assembly {assName}...");
+
+			AssemblyDefinition assembly = null;
+
+			if (!File.Exists(input))
+			{
+				Console.WriteLine();
+				Console.WriteLine("ERROR! File doesn't exist or you don't have sufficient permissions.");
+				Exit(30);
+			}
+
+			try
+			{
+				assembly = AssemblyDefinition.ReadAssembly((string)input);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine();
+				Console.WriteLine("ERROR! Cannot read the assembly. Please check your permissions.");
+				Exit(40);
+			}
+
+
+			var allTypes = GetAllTypes(assembly.MainModule);
+			var allMethods = allTypes.SelectMany(t => t.Methods);
+
+			var allFields = FilterBackingEventFields(allTypes);
+
+			int count;
+			string reportString = "Changed {0} {1} to public.";
+
+			#region Make everything public
+
+			count = 0;
+			foreach (var type in allTypes)
+			{
+				if (!type?.IsPublic ?? false && !type.IsNestedPublic)
+				{
+					count++;
+					if (type.IsNested)
+						type.IsNestedPublic = true;
+					else
+						type.IsPublic = true;
+				}
+			}
+
+			Console.WriteLine(reportString, count, "types");
+
+			count = 0;
+			foreach (var method in allMethods)
+			{
+				if (!method?.IsPublic ?? false)
+				{
+					count++;
+					method.IsPublic = true;
+				}
+			}
+
+			Console.WriteLine(reportString, count, "methods (including getters and setters)");
+
+			count = 0;
+			foreach (var field in allFields)
+			{
+				if (!field?.IsPublic ?? false)
+				{
+					count++;
+					field.IsPublic = true;
+				}
+			}
+
+			Console.WriteLine(reportString, count, "fields");
+
+			#endregion
+
+
+			Console.WriteLine();
+
+			Console.Write("Saving a copy of the modified assembly ...");
+
+			var outputFile = Path.Combine(outputPath, outputName);
+
+			try
+			{
+				if (outputPath != "" && !Directory.Exists(outputPath))
+					Directory.CreateDirectory(outputPath);
+				assembly.Write(outputFile);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine();
+				Console.WriteLine("ERROR! Cannot create/overwrite the new assembly. ");
+				Console.WriteLine("Please check the path and its permissions " +
+								  "and in case of overwriting an existing file ensure that it isn't currently used.");
+				Exit(50);
+			}
+
+			Console.WriteLine(" OK");
+			Console.WriteLine();
 		}
 
 		public static void Exit(int exitCode = 0)
